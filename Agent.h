0bc44e660ed9c5fc6a0ae3399c9ca34cc290c443
Wojtek Kryscinski <wkryscinski@gmail.com>
2014-11-16 21:27:31 +0100

    Comments added to Agent.h

--- Agent.h
+++ Agent.h
@@ -17,11 +17,25 @@
 
 class Agent {
 private:
+    /**
+     Value identifing an agent
+     */
     long agentId;
+    
+    /**
+     Traits of an agent
+     */
     std::vector<Trait> traits;
     
 public:
+    /**
+     Agent class default constructor
+     */
     Agent(long agentId);
+    
+    /**
+     Operator prints agent to stream.
+     */
     friend std::ostream& operator<< (std::ostream& os, const Agent& agent);
     friend double compareAgents(Agent& agentA, Agent& agentB);
 };
