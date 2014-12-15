03092c83b1ea25cbed42673cdd3868c83e25fa7a
Aleksander Wójcik <aleksander.wojcik@swmansion.com>
2014-12-15 21:52:08 +0100

    histo

---
+++ SimpleHistogram.cpp
@@ -0,0 +1,9 @@
+//
+//  SimpleHistogram.cpp
+//  Social Network
+//
+//  Created by Aleksander Wójcik on 15.12.2014.
+//  Copyright (c) 2014 Aleksander Wójcik. All rights reserved.
+//
+
+#include "SimpleHistogram.h"
---
+++ SimpleHistogram.h
@@ -0,0 +1,14 @@
+//
+//  SimpleHistogram.h
+//  Social Network
+//
+//  Created by Aleksander Wójcik on 15.12.2014.
+//  Copyright (c) 2014 Aleksander Wójcik. All rights reserved.
+//
+
+#ifndef __Social_Network__SimpleHistogram__
+#define __Social_Network__SimpleHistogram__
+
+#include <stdio.h>
+
+#endif /* defined(__Social_Network__SimpleHistogram__) */
